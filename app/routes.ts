ea62fa3f7ac36ee6b04ee9accb7fadba333df075
renanlucaz <renanluquinhas53@gmail.com>
2025-09-06 13:53:36 -0300

    clientes

--- app/routes.ts
+++ app/routes.ts
@@ -2,9 +2,10 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     route("/", "components/Layout.tsx", [
-        index("routes/dashboard.tsx"),
+        index("routes/dashboard.clientes.tsx"),
         route("clusters", "routes/clusters.tsx"),
         route("simular-leads", "routes/simular-leads.tsx"),
+        route("dashboard", "routes/dashboard.tsx"),
         route("dashboard/clientes", "routes/dashboard.clientes.tsx"),
         route("dashboard/nps", "routes/dashboard.nps.tsx"),
         route("dashboard/tickets", "routes/dashboard.tickets.tsx"),
